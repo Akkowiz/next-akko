e9fb7c63b19a44b2aead3b373f28458ae84030fc
Qingue <qingquestatus@outlook.com>
2024-11-04 13:41:43 +0100

    More Gnomes

--- app/components/page-components/gnome.tsx
+++ app/components/page-components/gnome.tsx
@@ -1,16 +1,23 @@
 "use client";
-import { useRef } from "react";
+import Image from "next/image";
+import GnomeIcon from "@/app/images/GnomeIcon.jpg";
 
 export default function Gnome() {
-  const getgnomed = new Audio("/getgnomed.mp3");
+  function getGnomeAudio() {
+    const rng = Math.floor(Math.random() * 6) + 1;
+    return new Audio(`/DE_GnomeSounds/GnomeMaleZanyGreetings0${rng}.ogg`);
+  }
 
   function handleClick() {
+    const getgnomed = getGnomeAudio(); // Get a random audio file
     getgnomed.play();
   }
 
   return (
     <>
-      <button onClick={handleClick}>Ooo</button>
+      <button onClick={handleClick}>
+        <Image src={GnomeIcon} alt="Button" />
+      </button>
     </>
   );
 }
