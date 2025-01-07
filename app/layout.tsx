10687c5732ad6ac36bef3107a839d1a4e3150085
Qingue <qingquestatus@outlook.com>
2025-01-07 09:54:33 +0100

    Edited metadata for a nicer preview

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,15 +4,47 @@ import NavBar from "./components/navigation-bar/NavBar";
 import "./globals.css";
 import { ThemeProvider } from "@/app/components/themeprovider/ThemeProvider";
 import { ReactNode } from "react";
+import PreviewIcon from "@/app/images/Preview.png";
 
 export const metadata: Metadata = {
   title: "Akko dot(on) Ninja",
-  description: "My own humble website",
+  description: "If you read this, remind me to read the fricking manual",
 };
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        <title>Akko dot(on) Ninja</title>
+        <meta
+          name="description"
+          content="If you read this, remind me to read the fricking manual"
+        />
+        <meta property="og:url" content="https://akko.ninja" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Akko dot(on) Ninja" />
+        <meta
+          property="og:description"
+          content="If you read this, remind me to read the fricking manual"
+        />
+        <meta
+          property="og:image"
+          content="https://opengraph.b-cdn.net/production/images/3431e8c5-8083-45f1-a512-3d50033741b4.png?token=1m5N2StKt_FpZqcrfqUOiDh4IYOP7aMQ9LP1MH9A-kU&height=630&width=1200&expires=33272239930"
+        />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="akko.ninja" />
+        <meta property="twitter:url" content="https://akko.ninja" />
+        <meta name="twitter:title" content="Akko dot(on) Ninja" />
+        <meta
+          name="twitter:description"
+          content="If you read this, remind me to read the fricking manual"
+        />
+        <meta
+          name="twitter:image"
+          content="https://opengraph.b-cdn.net/production/images/3431e8c5-8083-45f1-a512-3d50033741b4.png?token=1m5N2StKt_FpZqcrfqUOiDh4IYOP7aMQ9LP1MH9A-kU&height=630&width=1200&expires=33272239930"
+        />
+      </head>
       <body>
         <ThemeProvider>
           <NavBar />
