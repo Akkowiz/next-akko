1b815674c3a2a23208a3af9eaa4732802c56714b
Qingue <qingquestatus@outlook.com>
2025-01-14 09:42:27 +0100

    navbar is sized correctly now on mobile devices

--- app/components/navigation-bar/NavBar.tsx
+++ app/components/navigation-bar/NavBar.tsx
@@ -7,25 +7,22 @@ import Carousel from "./Carousel";
 export default function NavBar() {
   return (
     <>
-      <div className="navbar bg-base-200 h-20 outline outline-base-300/50 drop-shadow-lg">
-        <div className="navbar-start lg:space-x-2">
+      <div className="navbar bg-base-200 h-16 lg:h-20 p-0 outline outline-base-300/80">
+        <div className="navbar-start space-x-1">
           <Link href="/">
             <Image
               src={HomeIcon}
               alt="Home"
-              className="size-20 hover:opacity-60"
+              className="size-16 lg:size-20 lg:hover:opacity-60"
             />
           </Link>
           <ThemeController />
         </div>
-        <div className="navbar-center h-20 w-20 lg:w-60">
+        <div className="navbar-center h-16 lg:h-20 w-40 lg:w-60">
           <Carousel />
         </div>
         <div className="navbar-end">
           <ul className="menu menu-horizontal">
-            {/* <li>
-              <Link href="/placeholder">Placeholder</Link>
-            </li> */}
             <li>
               <Link href="/resources">Resources</Link>
             </li>
--- app/components/navigation-bar/ThemeController.tsx
+++ app/components/navigation-bar/ThemeController.tsx
@@ -9,23 +9,25 @@ export default function ThemeController() {
   const displayedTheme = theme.charAt(0).toUpperCase() + theme.slice(1);
 
   return (
-    <div className="dropdown">
-      <label tabIndex={0} className="btn bg-base-300">
-        <PaintBucket />
-        <span className="hidden lg:block">{displayedTheme}</span>
-      </label>
-      <ul
-        tabIndex={0}
-        className="dropdown-content menu bg-base-300 rounded-box"
-      >
-        {themesDaisy.map((theme) => (
-          <li key={theme}>
-            <a onClick={() => changeTheme(theme)}>
-              {theme.charAt(0).toUpperCase() + theme.slice(1)}
-            </a>
-          </li>
-        ))}
-      </ul>
-    </div>
+    <>
+      <div className="dropdown dropdown-hover">
+        <label tabIndex={0} className="btn bg-base-300">
+          <PaintBucket />
+          <span className="hidden lg:block">{displayedTheme}</span>
+        </label>
+        <ul
+          tabIndex={0}
+          className="dropdown-content menu bg-base-300 rounded-box shadow-lg z-50 opacity-100 visible"
+        >
+          {themesDaisy.map((theme) => (
+            <li key={theme}>
+              <a onClick={() => changeTheme(theme)}>
+                {theme.charAt(0).toUpperCase() + theme.slice(1)}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
   );
 }
