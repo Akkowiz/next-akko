6c2522d3b5a4ff15274796ddb1ce572c3dd78f92
Marc <marc@MacBookPro.telekom.ip>
2025-02-21 11:10:00 +0100

    setup quizpage

--- app/components/navigation-bar/NavBar.tsx
+++ app/components/navigation-bar/NavBar.tsx
@@ -5,30 +5,29 @@ import HomeIcon from "@/app/images/HutaoIcon.png";
 import Carousel from "./Carousel";
 
 export default function NavBar() {
-  return (
-    <>
-      <div className="navbar bg-base-200 h-16 lg:h-20 p-0 outline outline-base-300/80">
-        <div className="navbar-start space-x-1">
-          <Link href="/">
-            <Image
-              src={HomeIcon}
-              alt="Home"
-              className="size-16 lg:size-20 lg:hover:opacity-60"
-            />
-          </Link>
-          <ThemeController />
-        </div>
-        <div className="navbar-center h-16 lg:h-20 w-40 lg:w-60">
-          <Carousel />
-        </div>
-        <div className="navbar-end">
-          <ul className="menu menu-horizontal">
-            <li>
-              <Link href="/resources">Resources</Link>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div className="navbar bg-base-200 h-16 lg:h-20 p-0 outline outline-base-300/80">
+				<div className="navbar-start space-x-1">
+					<Link href="/">
+						<Image src={HomeIcon} alt="Home" className="size-16 lg:size-20 lg:hover:opacity-60" />
+					</Link>
+					<ThemeController />
+				</div>
+				<div className="navbar-center h-16 lg:h-20 w-40 lg:w-60">
+					<Carousel />
+				</div>
+				<div className="navbar-end">
+					<ul className="menu menu-horizontal">
+						<li>
+							<Link href="/quiz">Quiz</Link>
+						</li>
+						<li>
+							<Link href="/resources">Resources</Link>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</>
+	);
 }
---
+++ app/components/quiz/questions.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+// answers index-based!
+const frageAntwort = [
+	{
+		question: "123",
+		options: ["1", "2", "3", "4"],
+		answer: 0,
+	},
+	{
+		question: "123",
+		options: ["1", "2", "3", "4"],
+		answer: 3,
+	},
+	{
+		question: "123",
+		options: ["1", "2", "3", "4"],
+		answer: 2,
+	},
+	{
+		question: "123",
+		options: ["1", "2", "3", "4"],
+		answer: 1,
+	},
+	{
+		question: "123",
+		options: ["1", "2", "3", "4"],
+		answer: 3,
+	},
+	{
+		question: "123",
+		options: ["1", "2", "3", "4"],
+		answer: 1,
+	},
+	{
+		question: "123",
+		options: ["1", "2", "3", "4"],
+		answer: 0,
+	},
+];
+
+export default function Quiz() {
+	return <></>;
+}
