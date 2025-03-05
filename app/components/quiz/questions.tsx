56dc21fbfbc6fcf6b71c2cf333667ec35aa8d05e
Akkowiz <qingquestatus@outlook.com>
2025-03-05 08:50:49 +0100

    All selectable options are displayable now, next step will be calculating points

--- app/components/quiz/questions.tsx
+++ app/components/quiz/questions.tsx
@@ -3,18 +3,26 @@ import { frageAntwort } from "@/app/components/quiz/qa";
 
 const questionsAnswers = frageAntwort.map((item, index) => (
 	<li className="" key={index.toString()}>
-		<input type="radio" id={index.toString()} name="option" />
-		<label htmlFor={index.toString()}></label>
-		{item.question}
+		{item.question} <br />
+		{item.options.map((option, index) => (
+			<div key={index}>
+				<input type="radio" id={index.toString()} name="option" />
+				<label htmlFor={index.toString()}>{option}</label>
+			</div>
+		))}
 	</li>
 ));
 
+function Evaluation() {
+	alert("You clicked me!");
+}
+
 export default function Quizmaster() {
 	return (
-		<div className="border-pink-600">
+		<div>
 			<form>
 				<ul>{questionsAnswers}</ul>
-				<button type="submit">Confirm</button>
+				<button onClick={Evaluation}>Submit</button>
 			</form>
 		</div>
 	);
