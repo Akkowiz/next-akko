5fe22d7d2b85946cf6469ffcf33838d087effd23
Akkowiz <qingquestatus@outlook.com>
2025-03-06 12:28:08 +0100

    Answering questions and calculating points works now

--- app/components/quiz/questions.tsx
+++ app/components/quiz/questions.tsx
@@ -1,20 +1,37 @@
 "use client";
 import { frageAntwort } from "@/app/components/quiz/qa";
+let totalPoints = frageAntwort.length;
 
-const questionsAnswers = frageAntwort.map((item, index) => (
-	<li className="" key={index.toString()}>
+const questionsAnswers = frageAntwort.map((item, index1) => (
+	<li className="" key={index1.toString()}>
 		{item.question} <br />
-		{item.options.map((option, index) => (
-			<div key={index}>
-				<input type="radio" id={index.toString()} name="option" />
-				<label htmlFor={index.toString()}>{option}</label>
+		{item.options.map((option, index2) => (
+			<div key={index2}>
+				<input type="radio" id={`button-${index1}-${index2}`} name={`option-${index1}`} />
+				<label htmlFor={index2.toString()}>{option}</label>
 			</div>
 		))}
 	</li>
 ));
 
+function Result() {
+	let userPoints = Evaluation();
+	let result = document.getElementById("result")!;
+	result.innerHTML = `You scored ${userPoints} out of ${totalPoints} points!`;
+	result.removeAttribute("hidden");
+}
+
 function Evaluation() {
-	alert("You clicked me!");
+	let userPoints = 0;
+
+	for (let i = 0; i < frageAntwort.length; i++) {
+		let solution = frageAntwort[i].answer;
+		const currentButton = document.getElementById(`button-${i}-${solution}`) as HTMLInputElement;
+		if (currentButton.checked == true) {
+			userPoints++;
+		}
+	}
+	return userPoints;
 }
 
 export default function Quizmaster() {
@@ -22,8 +39,11 @@ export default function Quizmaster() {
 		<div>
 			<form>
 				<ul>{questionsAnswers}</ul>
-				<button onClick={Evaluation}>Submit</button>
+				<button type="button" onClick={Result}>
+					Submit
+				</button>
 			</form>
+			<p id="result" hidden></p>
 		</div>
 	);
 }
