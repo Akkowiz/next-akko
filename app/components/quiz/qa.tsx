7c96126aa59e91a38155b5e11d18ab89a74f4202
Akkowiz <qingquestatus@outlook.com>
2025-08-11 18:47:10 +0200

    Happy Birthday again

---
+++ app/components/quiz/qa.tsx
@@ -0,0 +1,130 @@
+export const frageAntwort = [
+	{
+		question: "1: ____ is my favourite tank in Final Fantasy XIV!",
+		options: ["Paladin", "Warrior", "Dark Knight", "Gunbreaker"],
+		answer: 3,
+	},
+
+	{
+		question: "2: My favourite DPS has to be ____",
+		options: ["Ninja", "Samurai", "Viper", "Black Mage"],
+		answer: 2,
+	},
+	{
+		question:
+			"3: Which uncconventional pizza topping do i crown as the best?",
+		options: [
+			"Apples, bacon, caramelised onions and cheddar",
+			"Broccoli, corn and hollandaise sauce",
+			"Kebab Pizza",
+			"Schnitzel stripes, red onions, hollandaise sauce and broccoli",
+		],
+		answer: 1,
+	},
+	{
+		question: "4: My green spirit animal:",
+		options: [
+			"Borpa",
+			"Dat Boi (the frog on the unicycle xD)",
+			"Glorpcat",
+			"Pepe (or Peepo)",
+		],
+		answer: [0, 2],
+	},
+	{
+		question: "5: There is ... inside of me.",
+		options: [
+			"a wolf",
+			"a pair of wolves who are both gay",
+			"an ice fairy",
+			"a skeleton that tries to take control",
+			"a skeleton that already took control",
+			"nothing",
+		],
+		answer: [2, 3],
+	},
+	{
+		question: "6.1: I think I'm ...",
+		options: [
+			"Gojo (Jujutsu Kaisen)",
+			"Escanor (Seven Deadly Sins)",
+			"Lelouch (Code Geass)",
+			"Michael Kaiser (Blue Lock)",
+		],
+		answer: [0, 3],
+	},
+	{
+		question: "6.2: ...but realistically I'm...",
+		options: [
+			"Deku's Mom (Boku no Hero Academia)",
+			"Griffith (Berserk)",
+			"Lain (Serial Experiments Lain)",
+			"Shinji (Evangelion)",
+		],
+		answer: 3,
+	},
+	{
+		question: "7: I hear/watch this when I want to laugh:",
+		options: [
+			"5 Minuten Harry Podcast",
+			"Brooklyn 99",
+			"Hobbylos",
+			"The Office",
+			"Quin69",
+		],
+		answer: [2, 4],
+	},
+	{
+		question: "8: I grew up playing lots of...",
+		options: [
+			"Jak and Daxter",
+			"Magic: The Gathering",
+			"Street Fighter",
+			"Zelda",
+		],
+		answer: 3,
+	},
+	{
+		question:
+			"9: An anime I like but saying it out loud is like asking for a ticket to jail:",
+		options: [
+			"Boku no Pico",
+			"Kissxsis",
+			"Oreimo",
+			"School Days",
+			"Yosuga no Sora",
+		],
+		answer: 1,
+	},
+	{
+		question: "10: My favourite Final Fantasy Protagonist is...",
+		options: ["Cloud", "Lightning", "Tidus", "Vaan"],
+		answer: 2,
+	},
+	{
+		question:
+			"11: The coolest maincharacter-persona in the Persona series (I never played P1 and P2):",
+		options: ["Orpheus", "Thanatos", "Izanagi", "Arsène"],
+		answer: 2,
+	},
+	{
+		question: "12: The question is simple:",
+		options: ["Asuka", "Rei"],
+		answer: 0,
+	},
+	{
+		question: "13: This question is simple aswell:",
+		options: ["Aerith", "Barret", "Tifa", "Yuffie"],
+		answer: 3,
+	},
+	{
+		question: "14: This guy does the coolest content on YouTube",
+		options: [
+			"Abroad in Japan",
+			"KBash",
+			"PrimeAgen",
+			"Super Eyepatch Wolf",
+		],
+		answer: 3,
+	},
+];
